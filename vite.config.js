15e1e01d0e9e4eec7d1fb74c1b791431c9b183a5
Ayushkumar <ayushkumar.patel@dcmail.ca>
2025-11-23 18:33:33 -0600

    change file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // for user page https://Ayushkumar1224.github.io/
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"), // @ = src
-    },
-  },
+  base: "/", // IMPORTANT FOR USERNAME GITHUB.IO
 });
